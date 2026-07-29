2cd52c54a4bb3241b6f0ae1e4ea9cb6cf448b8b2
이재광 <yellow@wordncode.com>
2026-07-29 12:08:53 +0900

    수정: Expo Go 실행 시 발생하던 ThemeProvider 임포트 모듈 렌더링 에러 수정

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import { useFonts } from 'expo-font';
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 
